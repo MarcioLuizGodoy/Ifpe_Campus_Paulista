97f9c10f75397e750ddbde74bd31eb9899d45624
MarcioLuizGodoy <marciooliveirasilvagodoy@gmail.com>
2023-12-04 23:09:01 -0300

    Adicionando os novos inserts ao codigo script principal

--- Projeto Bdd l/Suporte Ctech Script sql/Suporte_cetech_projeto.sql
+++ Projeto Bdd l/Suporte Ctech Script sql/Suporte_cetech_projeto.sql
@@ -118,6 +118,52 @@ insert into conselho_final_tecnico values (
     'nao permitir que o equipamento seja aberto por outro profissinal fora do suporte tecnico da prefeitura, o flat de video foi posto da forma errada e portanto foi danificado. Ademais, manter o notebook o mais estatico possavel porque ass portas usbs sao sensaveis',
     'zzzzkkk1234'
     );
+    
+    
+    
+    
+    insert into cliente_prof_telefones values (
+    '0000','444444'
+    );
+
+insert into equipamento_eletronico values (
+    '0000','0b1c','4321','Positivo'
+    );
+
+insert into tecnico_ctech values (
+    'Gutemberg','7777','2222'
+    );
+
+insert into conserto values (
+    '0b1c','7777'
+    );
+
+insert into estagiario values (
+    'amaro alguma coisa','6688','Gutember','7777'
+    );
+
+insert into documentacao values (
+    'aaaabbb4321','7777','0b1c',
+    'equipamento de ser substituido'
+    );
+
+insert into defeitos values (
+    'placa ruim','aaaabbb4321'
+    );
+
+insert into procedimentos_realizados values (
+    'Teste com Multimetro ,Troca de componentes',
+    'aaaabbb4321'
+    );
+
+insert into conselho_final_tecnico values (
+    'evitar comer e beber quando proximo ao equipamento porque tinha restos de comida e cafe ja ressecado na placa',
+    'aaaabbb4321'
+    ); 
+    
+    
+    
+    
 
 select * from documentacao;
 
